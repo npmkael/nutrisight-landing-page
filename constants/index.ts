f647016124b1293ec594be435ab2d48baeacf3a8
kieldoescode30 <kielycarreon123@gmail.com>
2025-11-18 13:08:20 +0800

    feat: new dl link

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
 export const APP_LINK =
-  "https://github.com/npmkael/nutrisight/releases/download/v1.0.5/nutrisight_1.0.5.apk";
+  "https://github.com/npmkael/nutrisight/releases/download/v1.0.7/nutrisight_1.0.7.apk";
